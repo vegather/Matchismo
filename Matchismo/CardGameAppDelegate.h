b94368c4a24147705594d72dc202fc5f665264f0
Vegard Solheim Theriault <vegather@gmail.com>
2013-01-30 10:47:54 +0100

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Vegard Solheim Thériault on 1/30/13.
+//  Copyright (c) 2013 Vegard Solheim Thériault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Vegard Solheim Thériault on 1/30/13.
+//  Copyright (c) 2013 Vegard Solheim Thériault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
