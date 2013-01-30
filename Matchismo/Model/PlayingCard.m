142d1db8aa8240e48d32d044ab33665bcc054af4
Vegard Solheim Theriault <vegather@gmail.com>
2013-01-30 12:21:21 +0100

    Created a private class method called rankStrings

--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -13,13 +13,18 @@
 
 - (NSString *)content
 {
-    NSArray *rankStrings = @[@"?", @"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"];
+    NSArray *rankStrings = [PlayingCard rankStrings];
     return [rankStrings[self.rank] stringByAppendingString:self.suit];
 }
 
 + (NSArray *)validSuits
 {
     return @[@"♥", @"♦", @"♠", @"♣"];
+}
+
++ (NSArray *)rankStrings
+{
+    return @[@"?", @"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"];
 }
 
 - (void)setSuit:(NSString *)suit
