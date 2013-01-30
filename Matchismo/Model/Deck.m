574ccbf230f82d62f738d93ace8581efcb612e53
Vegard Solheim Theriault <vegather@gmail.com>
2013-01-30 11:54:10 +0100

    Implemented addCard:atTop: and lazily instantiated NSMutableArray cards

--- Matchismo/Model/Deck.m
+++ Matchismo/Model/Deck.m
@@ -8,11 +8,31 @@
 
 #import "Deck.h"
 
+@interface Deck ()
+@property (strong, nonatomic) NSMutableArray *cards;
+@end
+
 @implementation Deck
+
+- (NSMutableArray *)cards
+{
+    if (!_cards)
+    {
+        _cards = [[NSMutableArray alloc]init];
+    }
+    return _cards;
+}
 
 - (void)addCard:(Card *)card atTop:(BOOL)atTop
 {
-    
+    if (atTop)
+    {
+        [self.cards insertObject:card atIndex:0];
+    }
+    else
+    {
+        [self.cards addObject:card];
+    }
 }
 
 - (Card *)drawRandomCard
