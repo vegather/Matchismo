a7339abb656ceaed3a1fbfbcf1542718e62910c3
Vegard Solheim Theriault <vegather@gmail.com>
2013-01-31 12:15:37 +0100

    Completed implementation of flipCardAtIndex:

--- Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Model/CardMatchingGame.m
@@ -59,18 +59,38 @@
 
 #pragma mark - Public Instance Methods
 
+#define MATCH_BONUS 4
+#define MISMATCH_PENALTY 2
+#define FLIP_COST 1
 - (void)flipCardAtIndex:(NSUInteger)index
 {
     Card *card = [self cardAtIndex:index];
     
     if (card && !card.isUnplayable)
     {
-        for (Card *otherCard in self.cards)
+        if (!card.isFaceUp)
         {
-            if (otherCard.isFaceUp && !otherCard.isUnplayable)
+            for (Card *otherCard in self.cards)
             {
+                if (otherCard.isFaceUp && !otherCard.isUnplayable)
+                {
+                    int matchScore = [card match:@[otherCard]];
+                    if (matchScore != 0)
+                    {
+                        card.unPlayable = YES;
+                        otherCard.unPlayable = YES;
+                        self.score += matchScore * MATCH_BONUS;
+                    }
+                    else
+                    {
+                        otherCard.faceUp = NO;
+                        self.score -= MISMATCH_PENALTY;
+                    }
+                    break;
+                }
                 
             }
+            self.score -= FLIP_COST;
         }
         card.faceUp = !card.faceUp;
     }
